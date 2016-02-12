f02b7d3001832afdf7453ba8d5ba6fa61066e408
Alex Tenche <alex.tenche@gmail.com>
2016-02-12 20:36:30 +0200

    code review

--- classes/Core.php
+++ classes/Core.php
@@ -1,9 +1,8 @@
 <?php
 class Core {
-	
+
 	public function run() {
-		// output buffering
-		ob_start();  
+		ob_start();
 		require_once( Url::getPage() );
 		ob_get_flush();
 	}
--- pages/error.php
+++ pages/error.php
@@ -1,4 +1,4 @@
 <?php require_once('_header.php'); ?>
-<h1>Page not found!</h1>
-<p>The page you are trying to access does not exist or has been moved.</p>
+<h1> Page not found! </h1>
+<p> The page you are trying to access does not exist or has been moved. </p>
 <?php require_once('_footer.php'); ?>
